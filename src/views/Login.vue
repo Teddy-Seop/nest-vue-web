41761ce2b3987489df2bdc044049a6e5419fcc3b
Teddy-Seop <teddy.kim@onda.me>
2020-10-08 11:24:45 +0900

    Add login validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Login Page</h1>
     <v-form>
-      <v-text-field v-model="email" label="Email"></v-text-field>
-      <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+      <v-text-field v-model.trim="email" label="Email" :rules="valdateEmail()"></v-text-field>
+      <v-text-field v-model.trim="password" label="Password" type="password" :rules="validatePassword()"></v-text-field>
       <v-btn @click="submit">Login</v-btn>
     </v-form>
   </div>
@@ -12,25 +12,53 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { Validate } from 'vuelidate-property-decorators';
+import { required, email } from 'vuelidate/lib/validators'
 
 @Component
 export default class Login extends Vue {
+  @Validate({ required, email })
   private email = "";
+
+  @Validate({ required })
   private password = "";
 
   private async submit() {
-    const result = await this.$store.dispatch("login", {
-      email: this.email,
-      password: this.password
-    });
-
-    if (this.$store.getters.getUserInfo.id !== '') {
-      localStorage.setItem("userId", this.$store.getters.getUserInfo.id);
-      Vue.$cookies.set("access_token", this.$store.getters.getUserInfo.accessToken, "1h");
-      this.$router.push("home");
+    if (this.$v.$invalid === false) {
+      const result = await this.$store.dispatch("login", {
+        email: this.email,
+        password: this.password
+      });
+      if (this.$store.getters.getUserInfo.id !== '') {
+        localStorage.setItem("userId", this.$store.getters.getUserInfo.id);
+        Vue.$cookies.set("access_token", this.$store.getters.getUserInfo.accessToken, "1h");
+        this.$router.push("home");
+      }
     }
+  }
+
+  private valdateEmail() {
+    const error: string[] = [];
+    if (!this.$v.email.required) {
+      error.push("Email is required");
+    } else if (!this.$v.email.email) {
+      error.push("Fomat should be email");
+    }
+    return error;
+  }
+
+  private validatePassword() {
+    const error: string[] = [];
+    if (!this.$v.password.required) {
+      error.push("Password is required");
+    }
+    return error;
   }
 }
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.invalid {
+  color: red;
+}
+</style>
